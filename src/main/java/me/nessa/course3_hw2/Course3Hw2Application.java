8d95798db4769ea666920e33f50f0f537b7ffc93
vanessa <vanesska_denis2011@mail.ru>
2023-01-02 15:01:00 +0300

    The first commit

---
+++ src/main/java/me/nessa/course3_hw2/Course3Hw2Application.java
@@ -0,0 +1,13 @@
+package me.nessa.course3_hw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course3Hw2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course3Hw2Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/nessa/course3_hw2/Course3Hw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nessa.course3_hw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course3Hw2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
